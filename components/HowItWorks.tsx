14d49bf4ab09c0f011dd0cce8f0450e0d5c4aa66
Rendyest <lbjz@outlook.com>
2026-08-11 06:00:51 -0600

    Update code to use progress image instead of CSS visual

--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -12,10 +12,11 @@ function StepVisual({ mode }: { mode: string }) {
         <img src={`${basePath}/images/move-naturally.gif`} alt="Move naturally" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       ) : mode === 'feedback' ? (
         <img src={`${basePath}/images/get-feedback.jpg`} alt="Get feedback as you move" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+      ) : mode === 'progress' ? (
+        <img src={`${basePath}/images/progress.jpg`} alt="Understand your progress" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
       ) : (
         <div className="mini-mirror">
           <div className="mini-person"><i></i><b></b></div>
-          {mode === 'progress' && <div className="progress-bars"><i></i><i></i><i></i><i></i></div>}
         </div>
       )}
     </div>
